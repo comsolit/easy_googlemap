12ac26faf455365dc9f518b42b981bffccfa9882
Robert Pupel <pupel@comsolit.com>
2015-07-14 15:27:56 +0200

    register coordinate-eval function

--- ext_localconf.php
+++ ext_localconf.php
@@ -3,6 +3,8 @@ if (!defined('TYPO3_MODE')) {
 	die ('Access denied.');
 }
 
+$TYPO3_CONF_VARS['SC_OPTIONS']['tce']['formevals']['tx_easygooglemap_coordinate'] = 'EXT:easy_googlemap/Configuration/TCA/class.tx_easygooglemap_coordinate.php';
+
 \TYPO3\CMS\Extbase\Utility\ExtensionUtility::configurePlugin(
 	'TYPO3.' . $_EXTKEY,
 	'Feeasygooglemap',
