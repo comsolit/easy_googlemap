4faf5a86ef6b7cde8dee7b760a421b7cff25608d
Robert Pupel <pupel@comsolit.com>
2015-08-06 10:50:49 +0200

    removed deprecated code
    ticket: 4777

--- Classes/ViewHelpers/PageRenderer/AddJsFooterInlineCodeViewHelper.php
+++ Classes/ViewHelpers/PageRenderer/AddJsFooterInlineCodeViewHelper.php
@@ -2,21 +2,8 @@
 
 namespace TYPO3\EasyGooglemap\ViewHelpers\PageRenderer;
 
-
-class AddJsFooterInlineCodeViewHelper extends \TYPO3\CMS\Fluid\Core\ViewHelper\AbstractViewHelper {
-
-    /**
-     *
-     * @var \TYPO3\CMS\Core\Page\PageRenderer
-     */
-    protected $pageRenderer;
-    /**
-     *
-     * @param \TYPO3\CMS\Core\Page\PageRenderer $pageRenderer
-     */
-    public function injectPageRenderer(\TYPO3\CMS\Core\Page\PageRenderer $pageRenderer) {
-        $this->pageRenderer = $pageRenderer;
-    }
+class AddJsFooterInlineCodeViewHelper extends \TYPO3\CMS\Fluid\Core\ViewHelper\AbstractViewHelper
+{
 
     /**
      *
@@ -25,9 +12,12 @@ class AddJsFooterInlineCodeViewHelper extends \TYPO3\CMS\Fluid\Core\ViewHelper\A
      * @param string $forceOnTop
      * @return NULL
      */
-    public function render($name, $compress = FALSE, $forceOnTop = FALSE) {
+    public function render($name, $compress = FALSE, $forceOnTop = FALSE)
+    {
+        $pageRenderer = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(\TYPO3\CMS\Core\Page\PageRenderer::class);
+
         $block = $this->renderChildren();
-        $this->pageRenderer->addJsFooterInlineCode($name, $block, $compress, $forceOnTop);
-        return NULL;
+        $pageRenderer->addJsFooterInlineCode($name, $block, $compress, $forceOnTop);
+        return null;
     }
 }
