05c5214a94c35676335e38c8aae2037789b9828a
Robert Pupel <robert.p1991@gmail.com>
2015-08-21 17:19:41 +0200

    load javascript and css files on page where extension is included

--- Classes/ViewHelpers/PageRenderer/AddJsFooterInlineCodeViewHelper.php
+++ Classes/ViewHelpers/PageRenderer/AddJsFooterInlineCodeViewHelper.php
@@ -2,9 +2,10 @@
 
 namespace TYPO3\EasyGooglemap\ViewHelpers\PageRenderer;
 
+use TYPO3\CMS\Core\Page\PageRenderer;
+
 class AddJsFooterInlineCodeViewHelper extends \TYPO3\CMS\Fluid\Core\ViewHelper\AbstractViewHelper
 {
-
     /**
      *
      * @param string $name
@@ -14,10 +15,21 @@ class AddJsFooterInlineCodeViewHelper extends \TYPO3\CMS\Fluid\Core\ViewHelper\A
      */
     public function render($name, $compress = FALSE, $forceOnTop = FALSE)
     {
-        $pageRenderer = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(\TYPO3\CMS\Core\Page\PageRenderer::class);
+        $pageRenderer = $this->getPageRenderer();
 
         $block = $this->renderChildren();
+        $pageRenderer->addJsLibrary('googlemap','http://maps.google.com/maps/api/js?v=3&sensor=false');
+        $pageRenderer->addJsLibrary('infobox', 'http://google-maps-utility-library-v3.googlecode.com/svn/trunk/infobox/src/infobox.js');
+        $pageRenderer->addCssFile(\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::extRelPath(easy_googlemap) . 'Resources/Public/css/map.css');
         $pageRenderer->addJsFooterInlineCode($name, $block, $compress, $forceOnTop);
         return null;
+    }
+
+    /**
+     * @return PageRenderer
+     */
+    private function getPageRenderer()
+    {
+        return \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(\TYPO3\CMS\Core\Page\PageRenderer::class);
     }
 }
