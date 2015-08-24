b938f9e0a2391ed88f3cbe3558541a5f1bee40ac
Robert Pupel <robert.p1991@gmail.com>
2015-08-24 16:40:29 +0200

    add url input userfunc

--- Classes/Userfuncs/Tca.php
+++ Classes/Userfuncs/Tca.php
@@ -12,4 +12,15 @@ class Tca
         $out [] = '<script type="text/javascript"' . ' src="' . \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::extRelPath(easy_googlemap) . 'Resources/Public/jquery/addressMapConfig.js">' . '</script>';
         return implode('', $out);
     }
+
+    public function urlInput($PA, $fObj)
+    {
+        $out [] = '<div class="form-control-wrap" style="max-width: 650px">';
+        $out [] = '<div class="form-control-clearable">';
+        $out [] = '<input type="hidden" class="url-input" value="' . $PA['row']['link'] . '" name="' . $PA['itemFormElName'] . '">';
+        $out [] = '</div>';
+        $out [] = '</div>';
+        $out [] = '<script type="text/javascript"' . ' src="' . \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::extRelPath(easy_googlemap) . 'Resources/Public/jquery/urlInput.js">' . '</script>';
+        return implode('', $out);
+    }
 }
---
+++ Resources/Public/jquery/urlInput.js
@@ -0,0 +1,83 @@
+(function() {
+    var $ = TYPO3.jQuery;
+    var hiddenField = $('.url-input');
+    var inputField = $('<input type="text" class="form-control">');
+    hiddenField.after(inputField);
+
+    var invalidElem = $('<p>URL is not valid</p>');
+    var validElem = $('<p>URL is valid</p>');
+
+    var protocols = ['http://', 'https://'];
+    var select = $('<select>');
+
+    protocols.forEach(function(protocol) {
+        select.append($('<option>').text(protocol));
+    });
+
+    invalidElem.css({
+        'color'       : 'red',
+        'display'     : 'inline',
+        'margin-left' : '6px'
+    });
+    validElem.css({
+        'color'       : 'green',
+        'display'     : 'inline',
+        'margin-left' : '6px'
+    });
+
+    inputField.before(select);
+    select.addClass('form-control');
+    select.css({
+        'display' : 'inline',
+        'top'      : '0',
+        'min-width': '20%',
+        'max-width': '20%'
+    });
+    select.addClass('form-control');
+
+    inputField.css({
+        'display' : 'inline',
+        'width'   : '50%'
+    });
+
+    inputField.after(invalidElem);
+    invalidElem.after(validElem);
+
+    invalidElem.hide();
+    validElem.hide();
+
+    var initialValue = hiddenField.val().split('://');
+
+    inputField.val(initialValue[1]);
+    select.find('option:contains(' + initialValue[0]+ '://' + ')').attr('selected', 'true');
+
+    function validateUrl(url) {
+        var regexp = new RegExp("^(www\\.)?([0-9A-Za-z-\\.@:%_\+~#=]+)+((\\.[a-zA-Z]{2,3})+)(/(.)*)?(\\?(.)*)?");
+        if (!regexp.test(url)) {
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+    function setValue()
+    {
+        if(validateUrl(inputField.val())) {
+            validElem.show();
+            invalidElem.hide();
+            hiddenField.val(select.find(":selected").text() + inputField.val());
+        } else {
+            invalidElem.show();
+            validElem.hide();
+            hiddenField.val('');
+        }
+    }
+
+    select.change(function(){
+        setValue();
+    });
+
+    inputField.on('change blur input', function() {
+        setValue();
+    });
+})();
