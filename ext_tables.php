b1d1ccf8853cf6e23836f955e097b12a39b8b083
Robert Pupel <robert.p1991@gmail.com>
2015-10-22 15:10:02 +0200

    change detecting typo3 version

--- ext_tables.php
+++ ext_tables.php
@@ -1,7 +1,5 @@
 <?php
 
-use TYPO3\CMS\Core\Utility\VersionNumberUtility;
-
 if (!defined('TYPO3_MODE')) {
 	die ('Access denied.');
 }
@@ -39,7 +37,7 @@ $TCA['tx_easygooglemap_domain_model_location'] = array(
 		),
 		'searchFields' => 'title, infobox, city, country, postal_code, street, anchorx, anchory, longitude, latitude, icon, link',
 		'dynamicConfigFile' => \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::extPath($_EXTKEY) . 'Configuration/TCA/Location.php',
-		'iconfile' => strpos(VersionNumberUtility::getCurrentTypo3Version(), '7') === 0
+		'iconfile' => \TYPO3\CMS\Core\Utility\GeneralUtility::compat_version('7.5')
 			? 'EXT:easy_googlemap/Resources/Public/Icons/tx_easygooglemap_domain_model_location.png'
 			: \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::extRelPath($_EXTKEY) . 'Resources/Public/Icons/tx_easygooglemap_domain_model_location.png'
 	),
