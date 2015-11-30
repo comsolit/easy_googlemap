56628fec840d1893a9886d8964be41813966399f
Lars Messmer <messmer@comsolit.com>
2015-11-30 18:07:15 +0100

    set version 1.2.6, 7.6 compatibility
    	modified:   Documentation/Changelog/Index.rst
    	modified:   ext_emconf.php

--- ext_emconf.php
+++ ext_emconf.php
@@ -27,13 +27,12 @@ $EM_CONF[$_EXTKEY] = array(
 	'modify_tables' => '',
 	'clearCacheOnLoad' => 0,
 	'lockType' => '',
-	'version' => '1.2.5',
+	'version' => '1.2.6',
 	'constraints' => array(
 		'depends' => array(
-			'extbase' => '6.0.99-7.5.99',
-			'fluid' => '6.0.99-7.5.99',
-			'typo3' => '6.0.99-7.5.99',
-			'css_styled_content' => ''
+			'extbase' => '6.0.99-7.6.99',
+			'fluid' => '6.0.99-7.6.99',
+			'typo3' => '6.0.99-7.6.99',
 		),
 		'conflicts' => array(
 		),
