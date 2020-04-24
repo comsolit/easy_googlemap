ded426a325d287649f65e14f2c9b719a1a5f9915
Nitin Parri <nitin@refresj.nl>
2020-04-24 09:20:07 +0200

    Update tt_content.php

--- Configuration/TCA/Overrides/tt_content.php
+++ Configuration/TCA/Overrides/tt_content.php
@@ -3,7 +3,7 @@
 defined('TYPO3_MODE') or die('Access denied.');
 
 \TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerPlugin(
-        'easy_googlemap',
-        'Feeasygooglemap',
-        'EasyGoogleMap'
-    );
+    'Comsolit.easy_googlemap',
+    'Feeasygooglemap',
+    'EasyGoogleMap'
+);
