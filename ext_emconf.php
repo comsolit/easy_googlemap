246f7e60634412b0f16bf20c3c57f39dc8caaa3e
Nitin Parri <nitin@refresj.nl>
2020-04-24 09:28:11 +0200

    Compatibility update for TYPO3 10.4

--- ext_emconf.php
+++ ext_emconf.php
@@ -1,30 +1,40 @@
 <?php
 
-$EM_CONF[$_EXTKEY] = array(
-    'title' => 'EasyGoogleMap',
-    'description' => 'Easy and simple Googlemap Extension for the TYPO3 CMS, Good use for company roadmap on websites and for responsive pages, based on the google maps API',
-    'category' => 'misc',
-    'author' => 'Andres Lobacovs, comsolit AG',
-    'author_email' => 'info@comsolit.ch',
-    'author_company' => 'comsolit AG',
-    'shy' => '',
-    'priority' => '',
-    'module' => '',
-    'state' => 'stable',
-    'internal' => '',
-    'uploadfolder' => '1',
-    'createDirs' => '',
-    'modify_tables' => '',
-    'clearCacheOnLoad' => 0,
-    'lockType' => '',
-    'version' => '3.0.1',
-    'constraints' => array(
-        'depends' => array(
-            'extbase' => '9.0.00-9.5.99',
-            'fluid' => '9.0.00-9.5.99',
-            'typo3' => '9.0.00-9.5.99',
-        ),
-        'conflicts' => array(),
-        'suggests' => array(),
+/***************************************************************
+ * Extension Manager/Repository config file for ext "easy_googlemap".
+ *
+ * Auto generated 23-04-2020 19:06
+ *
+ * Manual updates:
+ * Only the data in the array - everything else is removed by next
+ * writing. "version" and "dependencies" must not be touched!
+ ***************************************************************/
+
+$EM_CONF[$_EXTKEY] = array (
+  'title' => 'EasyGoogleMap',
+  'description' => 'Easy and simple Googlemap Extension for the TYPO3 CMS, Good use for company roadmap on websites and for responsive pages, based on the google maps API',
+  'category' => 'misc',
+  'author' => 'Andres Lobacovs, comsolit AG',
+  'author_email' => 'info@comsolit.ch',
+  'author_company' => 'comsolit AG',
+  'state' => 'stable',
+  'uploadfolder' => true,
+  'clearCacheOnLoad' => 0,
+  'version' => '3.0.2',
+  'constraints' => 
+  array (
+    'depends' => 
+    array (
+      'extbase' => '9.0.00-10.4.99',
+      'fluid' => '9.0.00-10.4.99',
+      'typo3' => '9.0.00-10.4.99',
     ),
+    'conflicts' => 
+    array (
+    ),
+    'suggests' => 
+    array (
+    ),
+  ),
+  'clearcacheonload' => false,
 );
