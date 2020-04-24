c155964f4b24fa742919c451574baed7512b66eb
Nitin Parri <nitin@refresj.nl>
2020-04-24 09:12:25 +0200

    Update ext_localconf.php

--- ext_localconf.php
+++ ext_localconf.php
@@ -6,9 +6,20 @@ if (!defined('TYPO3_MODE')) {
 $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['tce']['formevals']['Comsolit\\EasyGooglemap\\Evaluation\\CoordinateEvaluation'] = '';
 
 \TYPO3\CMS\Extbase\Utility\ExtensionUtility::configurePlugin(
-    'Comsolit.' . $_EXTKEY,
+    'Comsolit.easy_googlemap',
     'Feeasygooglemap',
     array(
         'Location' => 'list'
     )
 );
+
+$GLOBALS['TYPO3_CONF_VARS']['SYS']['formEngine']['nodeRegistry'][1587664120] = [
+    'nodeName' => 'coordinateResolver',
+    'priority' => 40,
+    'class' => \Comsolit\EasyGooglemap\Form\Element\CoordinateResolverElement::class,
+];
+$GLOBALS['TYPO3_CONF_VARS']['SYS']['formEngine']['nodeRegistry'][1587664121] = [
+    'nodeName' => 'urlInput',
+    'priority' => 40,
+    'class' => \Comsolit\EasyGooglemap\Form\Element\UrlInputElement::class,
+];
